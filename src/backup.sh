e3ad6f1d71820181c2e1752642f23dd19d14fa70
Florian Marending <florian@marending.dev>
2025-05-04 16:57:01 +0200

    Remove bucket in file name

--- src/backup.sh
+++ src/backup.sh
@@ -71,7 +71,7 @@ pg_dump --format=custom \
 SIZE=$(ls -nlt db.dump | head -n1 | awk '{print $5}')
 
 timestamp=$(date +"%Y-%m-%dT%H:%M:%S")
-s3_uri_base="s3://${S3_BUCKET}/${S3_PREFIX}/${POSTGRES_DATABASE}_${timestamp}.dump"
+s3_uri_base="s3://${S3_PREFIX}/${POSTGRES_DATABASE}_${timestamp}.dump"
 
 if [ -n "$PASSPHRASE" ]; then
   echo "Encrypting backup..."
